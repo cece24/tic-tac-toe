cf94e12f86be45c76c974de63e61d364711bc48a
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-13 23:34:45 -0500

    Alternate between adding crosses and noughts, add var to keep track

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   var squares = document.querySelectorAll('.square div');
+  var lastClassAdded = "nought";
 
   squares.forEach(function(square) {
     square.addEventListener('click', function() {
       this.classList.remove('empty');
-      this.classList.add('nought');
+      if (lastClassAdded === "nought") {
+        this.classList.add('cross');
+        lastClassAdded = "cross";
+      } else {
+        this.classList.add('nought');
+        lastClassAdded = "nought";
+      }
     })
   })
 })
